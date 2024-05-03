2131ebe259a3be099e9c9e24e93e82200cfa1b0c
Reda Dali <reda.dali68@gmail.com>
2024-05-03 22:41:57 +0200

    push

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 
 int main()
 {
-    //application a;
-    //a.menuPrincipal();
-    std::vector<int> aps = {12, 1, 4, 7, 11, 15, 18, 21, 23, 25, 26, 28, 31};
+    application a;
+    a.menuPrincipal();
+    /*std::vector<int> aps = {12, 1, 4, 7, 11, 15, 18, 21, 23, 25, 26, 28, 31};
     std::vector<int> fs = {31, 2, 3, 0, 4, 9, 0, 4, 6, 8, 0, 8, 9, 11, 0, 3, 6, 0, 7, 8, 0, 8, 0, 10, 0, 0, 11, 0, 10, 12, 0, 0};
     Rang rg;
 
@@ -21,6 +21,6 @@ int main()
 
     for (int i = 1; i <= aps[0]; i++)
         std::cout << "rang[" << i << "] = " << rang[i] << std::endl;
-
+*/
     return 0;
 }
